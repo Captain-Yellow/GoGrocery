23c6646113d0b7b80d034c75ecfbc6124c11be83
Mohammad Afshar <neo.mohammad.afshar@gmail.com>
2023-12-01 13:19:15 +0330

    Initial Commit

---
+++ GoGrocery/GoGroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoGroceryApp.swift
+//  GoGrocery
+//
+//  Created by Mohammad Afshar on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoGroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
